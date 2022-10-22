3ad31ba6caaf34aca78c71264f0ec945709cbc6b
lecrosoft <lecrosoft@gmail.com>
2022-10-22 21:44:13 +0100

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+// import Hero from "./components/Hero/Hero";
+import Home from "./components/Home/Home";
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
