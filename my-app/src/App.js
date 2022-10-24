cafedc6e58eb8ba4469fb6801ae21ea81e21044a
lecrosoft <lecrosoft@gmail.com>
2022-10-24 10:56:32 +0100

    React-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 // import Hero from "./components/Hero/Hero";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
